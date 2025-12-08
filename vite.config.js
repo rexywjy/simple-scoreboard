f391af18addfe71c77ff067d952010a0901fd4d2
rexywjy <rexy.w.s.putro@gdplabs.id>
2025-12-08 16:58:05 +0700

    modify repo name Github Page Config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/scoreboard-ayat/', // Change this to your repo name
+  base: '/simple-scoreboard/', // Must match your GitHub repo name
 })
